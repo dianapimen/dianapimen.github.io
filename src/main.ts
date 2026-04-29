d13085f75235da948c5ed4e1c28018328bace8ef
diana <diana.pimen.n@gmail.com>
2026-04-29 08:37:55 -0600

    Compilación de producción para dianapimen.github.io

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { provideFirebaseApp, initializeApp, getApp } from '@angular/fire/app'; // Agregamos getApp
+import { provideFirebaseApp, initializeApp, getApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 
 bootstrapApplication(App, {
   providers: [
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // Usamos una función que asegure que Firestore sabe exactamente qué base de datos usar
+
     provideFirestore(() => {
         const app = getApp(); // Obtiene la app ya inicializada arriba
         return getFirestore(app); // Le pasa la app y el nombre con el guion
